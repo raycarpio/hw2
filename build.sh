8f1e8edb56baa5b8db6c356c68d3a0be694f8529
Ray <carpio.ray@gmail.com>
2019-02-10 21:58:24 -0800

    updating branch with build.sh

---
+++ build.sh
@@ -0,0 +1,3 @@
+cat templates/top.html docs/blog.html templates/bottom.html >> blog.html
+cat templates/top.html docs/contactme.html templates/bottom.html >> contactme.html
+cat templates/top.html docs/projects.html templates/bottom.html >> projects.html
